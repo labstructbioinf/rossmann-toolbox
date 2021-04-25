4ff08066c3f25c0bb2ab19b25b70007dd953a0dc
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2021-04-26 00:47:42 +0200

    Reorganize tests

--- tests/test_seq_predictions.py
+++ tests/test_seq_predictions.py
@@ -3,19 +3,34 @@ from rossmann_toolbox import RossmannToolbox
 from Bio import SeqIO
 
 
-class TestPredictions:
+class TestSeqPredictions:
 
-    def test_core_prediction(self):
-        data = {str(entry.id): str(entry.seq) for entry in SeqIO.parse('tests/data/test.fas', 'fasta')}
+    def test_multiple_core_predictions(self):
+        data = {str(entry.id): str(entry.seq) for entry in SeqIO.parse('test-data/fasta/core_benchmark.fas', 'fasta')}
         rtb = RossmannToolbox(use_gpu=False)
         preds = rtb.predict(data, mode='core')
         pr_arr = np.asarray([list(preds[key].values()) for key in data.keys()])
-        ref_arr = np.load('tests/data/test.npy')
+        ref_arr = np.load('test-data/ref/core_benchmark.npy')
         assert np.square(pr_arr.flatten() - ref_arr.flatten()).mean(axis=0) < 10e-5
 
-    def test_full_length_prediction(self):
-        data = {str(entry.id): str(entry.seq) for entry in SeqIO.parse('tests/data/test_full.fas', 'fasta')}
+    def test_core_detection(self):
+        data = {str(entry.id): str(entry.seq) for entry in SeqIO.parse('test-data/fasta/full_length.fas', 'fasta')}
         rtb = RossmannToolbox(use_gpu=False)
-        preds = rtb.predict(data, mode='seq')
-        pr_arr = np.asarray([list(preds[key].values()) for key in data.keys()])
-        assert 'CGPGKKVGIVGLGGIGSMGTLISKAMGAETYVISRSSR' in preds['1piw_A']['sequence']
+        cores = rtb.seq_detect_cores(data)
+        assert len(cores['3m6i_A'][0]) == 1 # One core should be found
+        beg, end, prob = cores['3m6i_A'][0][0]
+        assert data['3m6i_A'][beg:end] == 'AGVRLGDPVLICGAGPIGLITMLCAKAAGACPLVITDIDEGRL' # Match the sequence of detected core
+        pr_arr = cores['3m6i_A'][1]
+        ref_arr = np.load('test-data/ref/seq_full_length_core_dl.npy')
+        assert np.square(pr_arr.flatten() - ref_arr.flatten()).mean(axis=0) < 10e-5 # Predictions
+
+    def test_core_detection_evaluation_importance(self):
+        data = {str(entry.id): str(entry.seq) for entry in SeqIO.parse('test-data/fasta/full_length.fas', 'fasta')}
+        rtb = RossmannToolbox(use_gpu=False)
+        preds, importance = rtb.predict(data, mode='seq', core_detect_mode='dl', importance=True)
+        pr_imp_arr = np.asarray(list(importance['3m6i_A'].values()))
+        pr_arr = np.asarray(list(preds['3m6i_A'].values())[0:-1])
+        ref_imp_arr = np.load('test-data/ref/seq_full_length_detect_eval_importance.npy')
+        ref_arr = np.load('test-data/ref/seq_full_length_detect_eval.npy')
+        assert np.square(pr_arr.flatten() - ref_arr.flatten()).mean(axis=0) < 10e-5 # Predictions
+        assert np.square(pr_imp_arr.flatten() - ref_imp_arr.flatten()).mean(axis=0) < 10e-5 # Importances
